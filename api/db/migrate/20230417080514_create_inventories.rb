8fd19d934d07e62b0aa1dd1f3e4ab04bc721e9b5
Stiflerzak <hilarymwendia97@gmail.com>
2023-04-17 11:06:23 +0300

    added Inventory model

---
+++ api/db/migrate/20230417080514_create_inventories.rb
@@ -0,0 +1,13 @@
+class CreateInventories < ActiveRecord::Migration[7.0]
+  def change
+    create_table :inventories do |t|
+      t.references :organization, null: false, foreign_key: true
+      t.string :image
+      t.string :name
+      t.text :description
+      t.integer :quantity
+
+      t.timestamps
+    end
+  end
+end
